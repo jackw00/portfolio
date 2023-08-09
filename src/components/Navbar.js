468816cc387df067debbe7d96c055deb673dfc70
jackw00 <74882779+jackw00@users.noreply.github.com>
2023-08-09 17:07:29 -0400

    style changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@ export default function Navbar() {
   return (
     <div className="bg-gray-800 flex flex-wrap p-5 flex-row font-mono">
       <div className="">
-        <a href="#about" className="text-xl hover:text-gray-300 font-medium text-white">
+        <a href="#about" className="text-m sm:text-xl hover:text-gray-300 font-medium text-white">
           Jack Wardlaw
         </a>
-        <a href="#projects" className="mx-5 hover:text-gray-200">
+        <a href="#projects" className="mx-5 text-sm sm:text-xl hover:text-gray-200">
           Projects
         </a>
-        <a href="#skills" className="mr-5 hover:text-gray-200">
+        <a href="#skills" className="mr-5 text-sm sm:text-xl hover:text-gray-200">
           Skills
         </a>
         <a
           href="#contact"
-          className="hover:text-gray-200">
+          className="text-sm sm:text-xl hover:text-gray-200">
           Contact Me
         </a>
       </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,8 +2,8 @@ import { skills } from "../data";
 
 export default function Skills() {
     return (
-        <div id="skills" className="mb-20">
-            <div className="container px-5 py-10 mx-auto">
+        <div id="skills" className="mb-20 font-mono">
+            <div className="flex flex-col justify-center items-center px-5 py-10 mx-auto ">
                 <div className="text-center mb-20">
                     <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
                         Skills &amp; Technologies
@@ -15,7 +15,7 @@ export default function Skills() {
                 <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2 ">
                 {skills.map((skill) => (
                     <div key={skill} className="p-2 sm:w-1/5 w-full">
-                        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                        <div className="bg-gray-800 rounded flex p-4 h-full">
                             <span className="title-font font-medium text-white">
                                 {skill}
                             </span>
